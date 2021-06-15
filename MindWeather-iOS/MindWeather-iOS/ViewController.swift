bd2d31a255a516e59f119182d4605979fc8db4db
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-06-15 11:00:44 +0900

    add RxSwift & RxCocoa dependency

---
+++ MindWeather-iOS/MindWeather-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MindWeather-iOS
+//
+//  Created by 이승주 on 2021/06/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MindWeather-iOS/Pods/Target Support Files/Pods-MindWeather-iOS/Pods-MindWeather-iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MindWeather_iOS : NSObject
+@end
+@implementation PodsDummy_Pods_MindWeather_iOS
+@end
