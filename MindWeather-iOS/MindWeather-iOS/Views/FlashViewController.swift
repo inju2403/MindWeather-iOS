12fa9b69aa944be254ed6905467faeed907bd634
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-06-16 20:41:11 +0900

    add FlashViewController

--- MindWeather-iOS/MindWeather-iOS/Views/FlashViewController.swift
+++ MindWeather-iOS/MindWeather-iOS/Views/FlashViewController.swift
@@ -4,16 +4,18 @@
 //
 //  Created by 이승주 on 2021/06/15.
 //
-
 import UIKit
 
-class ViewController: UIViewController {
+class FlashViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.performSegue(withIdentifier: "moveSubIdentify", sender: self)
+        }
     }
-
-
+    
 }
 
---
+++ MindWeather-iOS/MindWeather-iOS/Views/SubViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SubViewController.swift
+//  MindWeather-iOS
+//
+//  Created by 이승주 on 2021/06/15.
+//
+
+import UIKit
+
+class SubViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
