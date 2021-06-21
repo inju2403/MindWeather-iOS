4949d46c01b2e7022450401baa82c46c9a86d224
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-06-21 09:38:30 +0900

    Create SignUpViewController and SignUp.storyboard

---
+++ MindWeather-iOS/MindWeather-iOS/Views/ViewController/SignUpViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpViewController.swift
+//  MindWeather-iOS
+//
+//  Created by 이승주 on 2021/06/21.
+//
+
+import UIKit
+import Alamofire
+
+class SignUpViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
