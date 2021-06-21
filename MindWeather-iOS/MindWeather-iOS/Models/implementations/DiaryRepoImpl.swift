29f25fa475737d9ba124105c6480a8bc2d631882
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-06-21 23:18:05 +0900

    add IDiaryRepository.swift and DiaryRepoImpl.swift

---
+++ MindWeather-iOS/MindWeather-iOS/Models/implementations/DiaryRepoImpl.swift
@@ -0,0 +1,28 @@
+//
+//  DiaryRepoImpl.swift
+//  MindWeather-iOS
+//
+//  Created by 이승주 on 2021/06/21.
+//
+
+import Foundation
+import Alamofire
+
+class DiaryRepoImpl: IDiaryRepository {
+    func getDiarys() -> [Diary] {
+        <#code#>
+    }
+    
+    func getDiaryById(diaryId: String) -> Diary {
+        <#code#>
+    }
+    
+    func updateDiary(content: Content, diaryId: String) {
+        <#code#>
+    }
+    
+    func deleteDiary(diaryId: String) {
+        <#code#>
+    }
+    
+}
---
+++ MindWeather-iOS/MindWeather-iOS/Models/repository/IDiaryRepository.swift
@@ -0,0 +1,15 @@
+//
+//  IDiaryRepository.swift
+//  MindWeather-iOS
+//
+//  Created by 이승주 on 2021/06/21.
+//
+
+import Foundation
+
+protocol IDiaryRepository {
+    func getDiarys() -> [Diary]
+    func getDiaryById(diaryId: String) -> Diary
+    func updateDiary(content: Content, diaryId: String)
+    func deleteDiary(diaryId: String)
+}
