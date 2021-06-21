653921c68167c5baf6e321b9c57f35221df7a563
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-06-21 13:48:01 +0900

    make branch from login screen to main screen

---
+++ MindWeather-iOS/MindWeather-iOS/Views/ViewController/MainTabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  MainTabBarController.swift
+//  MindWeather-iOS
+//
+//  Created by 이승주 on 2021/06/21.
+//
+
+import UIKit
+
+class MyTabBarViewController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
