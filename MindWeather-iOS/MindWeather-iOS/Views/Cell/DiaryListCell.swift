3d0ddeaff343f78906d62ebcec0e33760704879a
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-06-27 00:33:38 +0900

    add DiaryListCell

---
+++ MindWeather-iOS/MindWeather-iOS/Views/Cell/DiaryListCell.swift
@@ -0,0 +1,23 @@
+//
+//  DiaryListCell.swift
+//  MindWeather-iOS
+//
+//  Created by 이승주 on 2021/06/27.
+//
+
+import UIKit
+
+class DiaryListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
