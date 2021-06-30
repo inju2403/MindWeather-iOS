28f0a43037527efa291dc7d982d40c3f4c66b2d4
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-06-30 23:10:16 +0900

    enable close button in DiaryEdit

--- MindWeather-iOS/MindWeather-iOS/Views/ViewController/DiaryEditViewController.swift
+++ MindWeather-iOS/MindWeather-iOS/Views/ViewController/DiaryEditViewController.swift
@@ -13,7 +13,14 @@ import RxCocoa
 class DiaryEditViewController : UIViewController {
     
     
+    @IBOutlet weak var dateText: UILabel!
+    @IBOutlet weak var dayText: UILabel!
+    @IBOutlet weak var content: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    @IBAction func closeButtonPressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
     }
 }
