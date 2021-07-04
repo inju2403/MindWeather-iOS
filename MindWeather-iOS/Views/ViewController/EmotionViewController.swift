d250547df7eb5c553e03744782bdaaf05ff3b75c
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-07-04 12:35:32 +0900

    Update colors of emotion graph

--- MindWeather-iOS/Views/ViewController/EmotionViewController.swift
+++ MindWeather-iOS/Views/ViewController/EmotionViewController.swift
@@ -14,13 +14,13 @@ import Charts
 
 class EmotionViewController : UIViewController {
     
-    var brownColor = "#8B4513" // 텍스트 색상
+    var brownColor = 0x8B4513 // 텍스트 색상
 
-    var goldColor = "#FFA700" // 행복
-    var greyColor = "#BBBBBB" // 중립
-    var pupleColor = "#A575CD" // 걱정
-    var blueColor = "#42A5F5" // 슬픔
-    var redColor = "#FF4950" // 분노
+    var goldColor = 0xFFA700 // 행복
+    var greyColor = 0xBBBBBB // 중립
+    var pupleColor = 0xA575CD // 걱정
+    var blueColor = 0x42A5F5 // 슬픔
+    var redColor = 0xFF4950 // 분노
 
     
     @IBOutlet weak var emotionGraph: BarChartView!
@@ -33,12 +33,9 @@ class EmotionViewController : UIViewController {
         super.viewWillAppear(true)
         
         let emotions = ["행복", "중립", "걱정", "슬픔", "분노"]
-        let goals = [6, 8, 26, 30, 8, 10]
+        let goals = [36, 8, 26, 13, 18, 10]
         
         customizeChart(dataPoints: emotions, values: goals.map{ Double($0) })
-        
-        
-        
     }
     
     func customizeChart(dataPoints: [String], values: [Double]) {
@@ -64,14 +61,37 @@ class EmotionViewController : UIViewController {
     }
     
     private func colorsOfCharts(numbersOfColor: Int) -> [UIColor] {
-      var colors: [UIColor] = []
-      for _ in 0..<numbersOfColor {
-        let red = Double(arc4random_uniform(256))
-        let green = Double(arc4random_uniform(256))
-        let blue = Double(arc4random_uniform(256))
-        let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
-        colors.append(color)
-      }
-      return colors
+        let colors: [UIColor] = [UIColor(rgb: goldColor), UIColor(rgb: greyColor), UIColor(rgb: pupleColor), UIColor(rgb: blueColor), UIColor(rgb: redColor)]
+        return colors
+    }
+}
+
+extension UIColor {
+
+    convenience init(red: Int, green: Int, blue: Int, a: Int = 0xFF) {
+        self.init(
+            red: CGFloat(red) / 255.0,
+            green: CGFloat(green) / 255.0,
+            blue: CGFloat(blue) / 255.0,
+            alpha: CGFloat(a) / 255.0
+        )
+    }
+
+    convenience init(rgb: Int) {
+           self.init(
+               red: (rgb >> 16) & 0xFF,
+               green: (rgb >> 8) & 0xFF,
+               blue: rgb & 0xFF
+           )
+       }
+    // let's suppose alpha is the first component (ARGB)
+
+    convenience init(argb: Int) {
+        self.init(
+            red: (argb >> 16) & 0xFF,
+            green: (argb >> 8) & 0xFF,
+            blue: argb & 0xFF,
+            a: (argb >> 24) & 0xFF
+        )
     }
 }
