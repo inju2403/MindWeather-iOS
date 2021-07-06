ca88e18a354f85dfb0099a43435052ea6e60fc92
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-07-06 21:19:42 +0900

    set font for logo text

--- MindWeather-iOS/Views/ViewController/FlashViewController.swift
+++ MindWeather-iOS/Views/ViewController/FlashViewController.swift
@@ -8,10 +8,12 @@ import UIKit
 
 class FlashViewController: UIViewController {
 
+    @IBOutlet weak var logoText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        logoText.font = UIFont.AppleSDGothic(type: .nanumpen, size: 40)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -31,3 +33,25 @@ class FlashViewController: UIViewController {
     
 }
 
+extension UIFont {
+    class func AppleSDGothic(type: AppleSDGothicType, size: CGFloat) -> UIFont! {
+        guard let font = UIFont(name: type.name, size: size) else {
+            return nil
+        }
+        return font
+    }
+
+    public enum AppleSDGothicType {
+        case nanumpen
+        case hanna
+
+        var name: String {
+            switch self {
+            case .nanumpen:
+                return "nanumpen"
+            case .hanna:
+                return "BMHANNAAir_otf"
+            }
+        }
+    }
+}
