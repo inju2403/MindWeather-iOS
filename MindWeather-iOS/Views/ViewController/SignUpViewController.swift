cf3071a09647a1aa2b220801d2cef7515100ae44
inju2403 <56947879+inju2403@users.noreply.github.com>
2021-07-11 12:13:37 +0900

    add logic for confirming failures by applying regular expressions to the sign up screen

--- MindWeather-iOS/Views/ViewController/SignUpViewController.swift
+++ MindWeather-iOS/Views/ViewController/SignUpViewController.swift
@@ -37,27 +37,36 @@ class SignUpViewController : UIViewController {
             return
         }
         
-        let signUpRequest = SignUpRequest(username: username, email: email, password1: password1, password2: password2)
-        
-        AF.request("\(K.API_BASE_URL)auth/registration/",
-                   method: .post,
-                   parameters: signUpRequest,
-                   encoder: JSONParameterEncoder())
-                .responseDecodable(of: SignUpRequest.self) { response in
-                    debugPrint(response)
-                    switch response.response?.statusCode {
-                    case 201:
-                        print("회원가입 성공")
-                        self.dismiss(animated: true, completion: nil)
-                        break
-                    case 400:
-                        print("error")
-                        break
-                    default:
-                        break
+        if isValidEmail(email: email) == false {
+            showAlert(style: .alert, message: "이메일 형식을 확인해주세요", type: "failure")
+        } else if password1 != password2 {
+            showAlert(style: .alert, message: "비밀번호가 일치하지 않아요", type: "failure")
+        } else if isValidPassword(password: password1) == false {
+            showAlert(style: .alert, message: "비밀번호는 영문, 숫자, 특수문자를 조합해서 만들어 주세요", type: "failure")
+        } else {
+            let signUpRequest = SignUpRequest(username: username, email: email, password1: password1, password2: password2)
+            
+            AF.request("\(K.API_BASE_URL)auth/registration/",
+                       method: .post,
+                       parameters: signUpRequest,
+                       encoder: JSONParameterEncoder())
+                    .responseDecodable(of: SignUpRequest.self) { response in
+                        debugPrint(response)
+                        switch response.response?.statusCode {
+                        case 201:
+                            print("회원가입 성공")
+                            self.showAlert(style: .alert, message: "회원가입 성공", type: "success")
+                            break
+                        case 400:
+                            self.showAlert(style: .alert, message: "닉네임 혹은 이메일이 이미 존재해요", type: "failure")
+                            print("error")
+                            break
+                        default:
+                            break
+                        }
+                        
                     }
-                    
-                }
+        }
     }
     
     // 사용자가 바로 입력할 수 있도록 세팅
@@ -79,6 +88,45 @@ class SignUpViewController : UIViewController {
         passwordTextField.underlined()
         passwordVerificationTextField.underlined()
     }
+    
+    func isValidEmail(email: String) -> Bool {
+      let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+      let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+      return emailTest.evaluate(with: email)
+   }
+    
+    func isValidPassword(password : String) -> Bool {
+        //영문+숫자+특수문자 포함해서 8~50글자 사이의 text 체크하는 정규표현식
+        let passwordreg = ("^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-]).{8,50}")
+        let passwordtesting = NSPredicate(format: "SELF MATCHES %@", passwordreg)
+        return passwordtesting.evaluate(with: password)
+    }
+
+    
+    func showAlert(style: UIAlertController.Style, message: String, type: String) {
+        let titleFont = [NSAttributedString.Key.font: UIFont.AppleSDGothic(type: .NanumMyeongjo, size: 16)]
+        let titleAttrString = NSMutableAttributedString(string: message, attributes: titleFont)
+        
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: style)
+        alert.setValue(titleAttrString, forKey:"attributedTitle")
+        
+        let success = UIAlertAction(title: "확인", style: .default, handler: {_ in
+            self.dismiss(animated: true, completion: nil)
+        })
+        success.setValue(UIColor(rgb: K.brownColor), forKey: "titleTextColor")
+        
+        let failure = UIAlertAction(title: "확인", style: .default, handler: nil)
+        failure.setValue(UIColor(rgb: K.brownColor), forKey: "titleTextColor")
+        
+        if type == "success" {
+            alert.addAction(success)
+        } else if type == "failure" {
+            alert.addAction(failure)
+        }
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+
 }
 
 extension SignUpViewController: UITextFieldDelegate {
