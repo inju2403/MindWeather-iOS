b775bb87c7abce785149850496adf1e14262cb03
inju2403 <56947879+inju2403@users.noreply.github.com>
2022-02-20 14:58:52 +0900

    refactor: Create a new file for UIColor+Extension and add it

---
+++ MindWeather-iOS/Common/UIColor+Extension.swift
@@ -0,0 +1,37 @@
+//
+//  UIColor+Extension.swift
+//  MindWeather-iOS
+//
+//  Created by 이승주 on 2022/02/20.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int, a: Int = 0xFF) {
+        self.init(
+            red: CGFloat(red) / 255.0,
+            green: CGFloat(green) / 255.0,
+            blue: CGFloat(blue) / 255.0,
+            alpha: CGFloat(a) / 255.0
+        )
+    }
+
+    convenience init(rgb: Int) {
+           self.init(
+               red: (rgb >> 16) & 0xFF,
+               green: (rgb >> 8) & 0xFF,
+               blue: rgb & 0xFF
+           )
+       }
+    // let's suppose alpha is the first component (ARGB)
+
+    convenience init(argb: Int) {
+        self.init(
+            red: (argb >> 16) & 0xFF,
+            green: (argb >> 8) & 0xFF,
+            blue: argb & 0xFF,
+            a: (argb >> 24) & 0xFF
+        )
+    }
+}
--- MindWeather-iOS/Views/ViewController/EmotionViewController.swift
+++ MindWeather-iOS/Views/ViewController/EmotionViewController.swift
@@ -5,8 +5,6 @@
 //  Created by 이승주 on 2021/06/29.
 //
 
-import Foundation
-
 import UIKit
 import RxSwift
 import RxCocoa
@@ -241,35 +239,5 @@ class EmotionViewController : UIViewController {
             UIColor(rgb: K.redColor)
         ]
         return colors
-    }
-}
-
-extension UIColor {
-
-    convenience init(red: Int, green: Int, blue: Int, a: Int = 0xFF) {
-        self.init(
-            red: CGFloat(red) / 255.0,
-            green: CGFloat(green) / 255.0,
-            blue: CGFloat(blue) / 255.0,
-            alpha: CGFloat(a) / 255.0
-        )
-    }
-
-    convenience init(rgb: Int) {
-           self.init(
-               red: (rgb >> 16) & 0xFF,
-               green: (rgb >> 8) & 0xFF,
-               blue: rgb & 0xFF
-           )
-       }
-    // let's suppose alpha is the first component (ARGB)
-
-    convenience init(argb: Int) {
-        self.init(
-            red: (argb >> 16) & 0xFF,
-            green: (argb >> 8) & 0xFF,
-            blue: argb & 0xFF,
-            a: (argb >> 24) & 0xFF
-        )
     }
 }
